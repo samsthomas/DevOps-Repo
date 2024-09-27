94d88f4f2241f0f9eb1f90fee4ba2450a1453ce4
Sam Thomas <samsthomas@hotmail.co.uk>
2024-09-27 15:09:54 +0100

    updating ado script

--- scripts/mass-branch-create-ado.ps1
+++ scripts/mass-branch-create-ado.ps1
@@ -2,47 +2,88 @@
 
 Add-Type -AssemblyName System.Web
 
-#variables 
-
-$pat = "$env:ADO_PAT"
-$organization = "https://dev.azure.com/samsthomas"
-$project = [System.Web.HttpUtility]::UrlEncode("Sams DevOps Project")
+#defining variables
 
 $Repositories = @("ADO-Test-Repo-1", "ADO-Test-Repo-2")
+$newBranch = "test-branch-1"
+$baseBranch = "main"
 
-$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($pat)"))
+$organization = "Sams-DevOps-Project"
+$project = "Sams-DevOps-Project"
+$pat = "personalAccessToken"
 
-$headers = @{
-    Authorization=("Basic {0}" -f $base64AuthInfo)
-}
+$base64AuthInfo = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$pat"))
 
-$defaultBranch = "refs/heads/main"
-$NewBranch = "feature/test-release-branch"
+#setting headers
+$headers = @{ Authorization = "Basic $base64AuthInfo" }
 
-
-
-
+#for loop to iterate through each repository and create a new branch
 foreach ($repo in $Repositories) {
-    $repoUrl = "$organization/$project/_apis/git/repositories/$([System.Web.HttpUtility]::UrlEncode($repo))?api-version=7.0"
-    Write-Host "Attempting to access: $repoUrl"
-    $repoDetails = Invoke-RestMethod -Uri $repoUrl -Method Get -Headers $headers
-    $repoId = $repoDetails.id
-    $defaultBranch = $repoDetails.defaultBranch.Replace("refs/heads/", "")
+    # Get ID of the base branch
+    $url = "https://dev.azure.com/$organization/$project/_apis/git/repositories/$repo/refs?filter=heads/$baseBranch&api-version=7.1"
+    $baseBranchResponse = Invoke-RestMethod -Uri $url -ContentType "application/json" -headers $headers -Method GET
 
-    # Get the SHA of the default branch
-    $branchUrl = "$organization/$project/_apis/git/repositories/$repoId/refs?filter=heads/$defaultBranch&api-version=7.0"
-    $branchDetails = Invoke-RestMethod -Uri $branchUrl -Method Get -Headers $headers
-    $defaultBranchSha = $branchDetails.value[0].objectId
+    # Create a new branch
+    $url = "https://dev.azure.com/$organization/$project/_apis/git/repositories/$repo/refs?api-version=7.1"
+    $body = ConvertTo-Json @(
+        @{
+            name = "refs/heads/$newBranch"
+            newObjectId = $baseBranchResponse.value.objectId
+            oldObjectId = "0000000000000000000000000000000000000000"
+        }
+    )
 
-    $createBranchUrl = "$organization/$project/_apis/git/repositories/$repoId/refs?api-version=7.0"
-    $body = @{
-        name = "refs/heads/$NewBranch"
-        oldObjectId = "0000000000000000000000000000000000000000"
-        newObjectId = $defaultBranchSha
-    } | ConvertTo-Json
 
-    Invoke-RestMethod -Uri $createBranchUrl -Method Post -Headers $headers -Body $body -ContentType "application/json"
-    Write-Host "Created branch $NewBranch in repository $repo"
+    $response = Invoke-RestMethod -Uri $url -ContentType "application/json" -Body $body -headers $headers -Method POST
+
 }
+
+
+
+
+
+
+
+
+# $pat = "$env:ADO_PAT"
+# $organization = "https://dev.azure.com/samsthomas"
+# $project = [System.Web.HttpUtility]::UrlEncode("Sams DevOps Project")
+
+# $Repositories = @("ADO-Test-Repo-1", "ADO-Test-Repo-2")
+
+# $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($pat)"))
+
+# $headers = @{
+#     Authorization=("Basic {0}" -f $base64AuthInfo)
+# }
+
+# $defaultBranch = "refs/heads/main"
+# $NewBranch = "feature/test-release-branch"
+
+
+
+
+# foreach ($repo in $Repositories) {
+#     $repoUrl = "$organization/$project/_apis/git/repositories/$([System.Web.HttpUtility]::UrlEncode($repo))?api-version=7.0"
+#     Write-Host "Attempting to access: $repoUrl"
+#     $repoDetails = Invoke-RestMethod -Uri $repoUrl -Method Get -Headers $headers
+#     $repoId = $repoDetails.id
+#     $defaultBranch = $repoDetails.defaultBranch.Replace("refs/heads/", "")
+
+#     # Get the SHA of the default branch
+#     $branchUrl = "$organization/$project/_apis/git/repositories/$repoId/refs?filter=heads/$defaultBranch&api-version=7.0"
+#     $branchDetails = Invoke-RestMethod -Uri $branchUrl -Method Get -Headers $headers
+#     $defaultBranchSha = $branchDetails.value[0].objectId
+
+#     $createBranchUrl = "$organization/$project/_apis/git/repositories/$repoId/refs?api-version=7.0"
+#     $body = @{
+#         name = "refs/heads/$NewBranch"
+#         oldObjectId = "0000000000000000000000000000000000000000"
+#         newObjectId = $defaultBranchSha
+#     } | ConvertTo-Json
+
+#     Invoke-RestMethod -Uri $createBranchUrl -Method Post -Headers $headers -Body $body -ContentType "application/json"
+#     Write-Host "Created branch $NewBranch in repository $repo"
+# }
 
 
