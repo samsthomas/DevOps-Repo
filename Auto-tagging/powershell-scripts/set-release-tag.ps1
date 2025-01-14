1bd59f355a50bfff1aa510e4abd0ee2238acad28
Sam Thomas <samsthomas@hotmail.co.uk>
2025-01-14 22:42:05 +0000

    added debugging

--- Auto-tagging/powershell-scripts/set-release-tag.ps1
+++ Auto-tagging/powershell-scripts/set-release-tag.ps1
@@ -1,7 +1,7 @@
 function getLatestVersionByMajor ($Major) {
     $listOfReleases = git tag -l 
 
-    $versions = $listOfReleases | Where-Object {$_ -match "^Release-$Major.[0-9]+.[0-9]+.[0-9]+$"} | Foreach-Object {[System.Version]::new($_.Substring(9))}
+    $versions = $listOfReleases | Where-Object {$_ -match "^Release-$Major.[0-9]+.[0-9]+$" } | Foreach-Object {[System.Version]::new($_.Substring(9))}
     $ver = $versions | Sort-Object -Descending | Select-Object -First 1
     return $ver
      
@@ -11,7 +11,7 @@ $versioningJsonPath = './Auto-tagging/version.json'
 
 function testIfTagPresent ($tag) {
     $listOfReleases = git tag -l 
-    $result = $listOfReleases | Where-Object { $_ -match "^$tags$"} | Select-Object -First 1
+    $result = $listOfReleases | Where-Object { $_ -match "^$tag$"} | Select-Object -First 1
     if ($result.count -eq 1){
         return $true
     }
@@ -33,7 +33,13 @@ catch {
 
 }
 
+Write-Host "Raw version info: $($versionInfo | ConvertTo-Json)"
+Write-Host "Major version: $($versionInfo.major)"
+
 $latestReleaseVersion = getLatestVersionByMajor $versionInfo.major
+
+Write-Host "Raw version info: $($versionInfo | ConvertTo-Json)"
+Write-Host "Major version: $($versionInfo.major)"
 
 Write-Host "Latest release version $($latestReleaseVersion)"
 
